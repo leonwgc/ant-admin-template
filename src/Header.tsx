207cad52b3e8e387bdc7892eaa96952677131cb5
leonwgc <leon.wang@derbysoft.net>
2025-01-23 17:37:30 +0800

    chore: show current permissions

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import { Layout, Avatar } from 'antd';
+import { Layout, Avatar, Space } from 'antd';
 import DerbySoftLogo from './Logo';
+import { useAppData } from 'simple-redux-store';
 
 const Header: React.FC<React.HTMLAttributes<HTMLElement>> = (props) => {
+  const { operations = [] } = useAppData();
   return (
     <Layout.Header {...props}>
       <DerbySoftLogo />
-      <Avatar>W</Avatar>
+
+      <Space size={64}>
+        <div>operations:{operations.map((op) => op).join(',')}</div>
+        <Avatar>W</Avatar>
+      </Space>
     </Layout.Header>
   );
 };
--- src/pages/AddTemplate.tsx
+++ src/pages/AddTemplate.tsx
@@ -4,6 +4,7 @@ export default () => {
   return (
     <div>
       <h1>Add Template</h1>
+      <p>This is the Add Template page.</p>
     </div>
   );
 };
