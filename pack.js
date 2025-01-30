a2e690923f8e4395bc05c41eb0d99c9eaa4f2cdf
leonwgc <giantfish@126.com>
2025-01-30 18:42:57 +0800

    chore: update packrs

--- pack.js
+++ pack.js
@@ -1,20 +1,7 @@
 const { run } = require('packrs');
-const path = require('path');
 
 run({
-  banner: 'admin',
-  index: './src/index.tsx',
-  dist: './dist',
-  port: 3000,
-  less: true,
-  sass: true,
   rsConfig: {
-    // resolve: {
-    //   aliasStrategy: 'prefer-alias',
-    //   alias: {
-    //     '~': path.resolve(__dirname, 'src'),
-    //   },
-    // },
     source: {
       define: {
         MOCK: JSON.stringify(true),
