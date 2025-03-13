cf02e23bc93e3022445baeedd2095746a4c79e4e
leonwgc <leon.wang@derbysoft.net>
2025-03-13 16:41:47 +0800

    chore: use e-sign modal

--- src/modules/Fadada/Eui.tsx
+++ src/modules/Fadada/Eui.tsx
@@ -1,6 +1,8 @@
-import { Button } from '@derbysoft/neat-design';
+import { Button } from 'antd';
+import { CloseOutlined } from '@derbysoft/neat-design-icons';
 import React, { useEffect, useRef } from 'react';
 import FullScreenModal from '~/components/FullScreenModal';
+import { Modal } from 'e-sign';
 
 export default () => {
   const [open, setOpen] = React.useState(false);
@@ -18,7 +20,7 @@ export default () => {
     <>
       <Button onClick={() => setOpen(true)}>Auth</Button>
 
-      <FullScreenModal
+      {/* <FullScreenModal
         open={open}
         onClose={() => {
           setOpen(false);
@@ -33,7 +35,34 @@ export default () => {
           }}
           src="https://80002452.uat-e.fadada.com/authorizeui/corp/login?authSerial=59cbdab84fec4f0791ccd0cdb417a275"
         />
-      </FullScreenModal>
+      </FullScreenModal> */}
+
+      <Modal
+        style={{ width: '100vw', height: '100vh', position: 'relative' }}
+        visible={open}
+        onClose={() => {
+          setOpen(false);
+        }}
+      >
+        <CloseOutlined
+          onClick={() => setOpen(false)}
+          style={{
+            fontSize: 20,
+            position: 'absolute',
+            right: 16,
+            top: 16,
+          }}
+        />
+        <iframe
+          ref={ref}
+          style={{
+            height: '100vh',
+            border: 'none',
+            width: '100%',
+          }}
+          src="https://80002452.uat-e.fadada.com/authorizeui/corp/login?authSerial=59cbdab84fec4f0791ccd0cdb417a275"
+        />
+      </Modal>
     </>
   );
 };
