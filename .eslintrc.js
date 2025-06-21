f712cdc498cd901ee06056ec7425d6d9140652c9
leonwgc <giantfish@126.com>
2025-06-21 14:33:27 +0800

    chore: update package.json to add linting scripts and dependencies
    - Added lint and lint:fix scripts for ESLint.
    - Included ESLint and related plugins for TypeScript and React.
    - Added Prettier for code formatting.

---
+++ .eslintrc.js
@@ -0,0 +1,60 @@
+module.exports = {
+  env: {
+    node: true,
+    browser: true,
+    commonjs: true,
+    es6: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+  },
+  rules: {
+    'no-prototype-builtins': 0,
+    'quotes': [2, 'single', 'avoid-escape'],
+    'react/react-in-jsx-scope': 0,
+    'react/no-unknown-property': [
+      2,
+      {
+        ignore: ['css'],
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+      ],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: true,
+      },
+      rules: {
+        '@typescript-eslint/ban-ts-comment': 0,
+        '@typescript-eslint/no-explicit-any': 0,
+        '@typescript-eslint/no-unsafe-argument': 0,
+        '@typescript-eslint/no-unsafe-assignment': 0,
+        '@typescript-eslint/no-unsafe-call': 0,
+        '@typescript-eslint/no-unsafe-member-access': 0,
+        '@typescript-eslint/no-unsafe-return': 0,
+        '@typescript-eslint/restrict-plus-operands': 0,
+        '@typescript-eslint/restrict-template-expressions': 0,
+        '@typescript-eslint/no-misused-promises': [
+          'error',
+          {
+            checksVoidReturn: false,
+          },
+        ],
+      },
+    },
+  ],
+};
