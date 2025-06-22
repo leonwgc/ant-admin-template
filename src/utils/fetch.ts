18b8bfe2b687261769b4009d2aa3ff8a20119164
leonwgc <giantfish@126.com>
2025-06-22 10:29:45 +0800

    refactor: add type annotations for Axios response handlers and bind fetch methods

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios, { AxiosResponse, AxiosError } from 'axios';
 
 // fetch normal api
 const fetch = axios.create({
@@ -28,23 +28,26 @@ const fetchDocuSign = axios.create({
   baseURL: 'https://demo.docusign.net/restapi',
 });
 
-const onSuccess = (response) => {
+const onSuccess = (response: AxiosResponse): AxiosResponse => {
   if (response.status >= 200 && response.status < 300) {
     return response;
   }
-  return Promise.reject(new Error(`请求失败，状态码: ${response.status}`));
 };
 
-const onFailure = (error) => {
+const onFailure = (error: AxiosError): Promise<never> => {
   if (error.response) {
     // 请求已发送，服务器返回了非 2xx 状态码
+    // eslint-disable-next-line no-console
     console.error('请求失败，服务器响应状态码:', error.response.status);
+    // eslint-disable-next-line no-console
     console.error('错误响应数据:', error.response.data);
   } else if (error.request) {
     // 请求已发送，但没有收到响应
+    // eslint-disable-next-line no-console
     console.error('请求已发送，但没有收到响应');
   } else {
     // 在设置请求时发生错误
+    // eslint-disable-next-line no-console
     console.error('设置请求时发生错误:', error.message);
   }
   return Promise.reject(error);
@@ -54,17 +57,17 @@ fetch.interceptors.response.use(onSuccess, onFailure);
 fetchProxy.interceptors.response.use(onSuccess, onFailure);
 fetchDocuSign.interceptors.response.use(onSuccess, onFailure);
 
-export const get = fetch.get;
-export const post = fetch.post;
-export const put = fetch.put;
-export const del = fetch.delete;
+export const get = fetch.get.bind(fetch);
+export const post = fetch.post.bind(fetch);
+export const put = fetch.put.bind(fetch);
+export const del = fetch.delete.bind(fetch);
 
-export const proxyGet = fetchProxy.get;
-export const proxyPost = fetchProxy.post;
-export const proxyPut = fetchProxy.put;
-export const proxyDel = fetchProxy.delete;
+export const proxyGet = fetchProxy.get.bind(fetchProxy);
+export const proxyPost = fetchProxy.post.bind(fetchProxy);
+export const proxyPut = fetchProxy.put.bind(fetchProxy);
+export const proxyDel = fetchProxy.delete.bind(fetchProxy);
 
-export const dsGet = fetchDocuSign.get;
-export const dsPost = fetchDocuSign.post;
-export const dsPut = fetchDocuSign.put;
-export const dsDel = fetchDocuSign.delete;
+export const dsGet = fetchDocuSign.get.bind(fetchDocuSign);
+export const dsPost = fetchDocuSign.post.bind(fetchDocuSign);
+export const dsPut = fetchDocuSign.put.bind(fetchDocuSign);
+export const dsDel = fetchDocuSign.delete.bind(fetchDocuSign);
