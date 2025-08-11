00f806c0940e8f21f7342568f39541900d938d2e
leonwgc <giantfish@126.com>
2025-08-11 21:46:44 +0800

    chore: add missing logLevel and title properties in pack.mjs configuration

--- pack.mjs
+++ pack.mjs
@@ -1,6 +1,7 @@
 import { run } from '@derbysoft/pack';
 
 run({
+  port: 3001,
   proxy: [
     {
       context: ['/book-engine-dev'],
@@ -9,21 +10,25 @@ run({
         '^/book-engine-dev': '',
       },
       changeOrigin: true,
+      logLevel: 'silent',
     },
     {
       context: ['/book-engine-qa'],
       target: 'https://unifyplatform.qa.derbysoft-test.com/',
       changeOrigin: true,
+      logLevel: 'silent',
     },
     {
       context: ['/book-engine-uat'],
       target: 'https://api-gateway2.derbysoft-test.com/',
       changeOrigin: true,
+      logLevel: 'silent',
     },
   ],
   rsConfig: {
     html: {
       template: './index.html',
+      title: '',
     },
   },
 });
