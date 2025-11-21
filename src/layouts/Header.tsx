3e4700e5240d172934b3bc0a787b7d1d4f055dc1
leonwgc <leon.wang@derbysoft.net>
2025-11-21 17:10:01 +0800

    feat: update Header component styles and structure; replace logo with AIRelatedFilled icon

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Layout, Avatar, Space, Flex } from '@derbysoft/neat-design';
-import DerbySoftLogo from './Logo';
-import { MenuOutlined } from '@ant-design/icons';
+// import DerbySoftLogo from './Logo';
+import { ExperimentOutlined, MenuOutlined } from '@ant-design/icons';
 import { useToggle } from 'ahooks';
 import MobileMenus from './MobileMenus';
 import { useBookEngineStore } from '~/store';
+import { AIRelatedFilled } from '@derbysoft/neat-design-icons';
 
 const Header: React.FC<React.HTMLAttributes<HTMLElement>> = (props) => {
   const { operations = [] } = useBookEngineStore();
@@ -15,15 +16,14 @@ const Header: React.FC<React.HTMLAttributes<HTMLElement>> = (props) => {
         <Flex
           align="center"
           gap={8}
-          style={{ fontSize: 14, fontWeight: 'bold' }}
+          style={{ fontSize: 32, fontWeight: 'bold' }}
         >
-          <DerbySoftLogo />
-          Ant Admin
+          <AIRelatedFilled />
         </Flex>
 
         <Space size={16}>
           <MenuOutlined className="mobile-menus" onClick={setRight} />
-          <Avatar>LW</Avatar>
+          {/* <Avatar>LW</Avatar> */}
         </Space>
       </Layout.Header>
       <MobileMenus open={open} onClose={setLeft} />
