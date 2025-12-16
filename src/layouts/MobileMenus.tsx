d640db827104ed62745fa97cf9bcb64c6c46b591
leonwgc <leon.wang@derbysoft.net>
2025-12-16 15:35:16 +0800

    refactor: remove unused useWindowSize hook and related effect in MobileMenus component

--- src/layouts/MobileMenus.tsx
+++ src/layouts/MobileMenus.tsx
@@ -1,19 +1,10 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Avatar, Drawer, DrawerProps } from '@derbysoft/neat-design';
 import Menus from './Menus';
-import useWindowSize from '~/hooks/useWindowSize';
 import { menus } from '~/config.menu';
 import './MobileMenus.scss';
 
 const MobileMenus: React.FC<DrawerProps> = (props) => {
-  const { width } = useWindowSize();
-
-  useEffect(() => {
-    if (width > 768) {
-      props.onClose?.(null);
-    }
-  }, [props, width]);
-
   const afterClick = () => {
     props.onClose?.(null);
   };
