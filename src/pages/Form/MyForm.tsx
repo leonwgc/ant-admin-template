9523a6c4e3d1690a016ba621a1fcb906dc0359ca
leonwgc <leon.wang@derbysoft.net>
2025-12-18 17:30:34 +0800

    feat: add MyForm component with responsive layout and styling enhancements

--- src/pages/Form/MyForm.tsx
+++ src/pages/Form/MyForm.tsx
@@ -11,7 +11,8 @@ import {
   Space,
 } from '@derbysoft/neat-design';
 import type { UploadProps } from '@derbysoft/neat-design';
-import './Form.scss';
+import './MyForm.scss';
+import { ConfigProvider } from '@derbysoft/neat-design';
 
 interface FormValues {
   firstName: string;
@@ -80,118 +81,130 @@ const Form: FC = () => {
           </div>
         </div>
       </div>
-
-      <NeatForm
-        form={form}
-        layout="vertical"
-        className="form-profile__form"
-        initialValues={{
-          firstName: 'Felicia',
-          lastName: 'Lawson',
-          primaryLanguage: 'English',
-          country: 'China',
-          timeZone: 'Asia/Shanghai',
-          workEmail: 'felicia.lawson@derbysoft.net',
-          secondaryEmail: '',
-          jobTitle: 'Product Manager',
-          phoneNumber: '+87 18018600000',
+      <ConfigProvider
+        prefixCls="ds"
+        theme={{
+          components: {
+            Form: {
+              'form-item-gap': 0,
+            },
+          },
         }}
       >
-        <div className="form-profile__section">
-          <div className="form-profile__section-title">Basic Info</div>
-          <Row gutter={24}>
-            <Col {...responsiveColDef}>
-              <NeatForm.Item
-                label="First Name"
-                name="firstName"
-                rules={[{ required: true, message: 'First Name is required' }]}
-              >
-                <Input />
-              </NeatForm.Item>
-            </Col>
-            <Col {...responsiveColDef}>
-              <NeatForm.Item
-                label="Last Name"
-                name="lastName"
-                rules={[{ required: true, message: 'Last Name is required' }]}
-              >
-                <Input />
-              </NeatForm.Item>
-            </Col>
-            <Col {...responsiveColDef}>
-              <NeatForm.Item label="Primary Language" name="primaryLanguage">
-                <Select options={languageOptions} />
-              </NeatForm.Item>
-            </Col>
-          </Row>
-          <Row gutter={24}>
-            <Col {...responsiveColDef}>
-              <NeatForm.Item
-                label="Country/Region"
-                name="country"
-                rules={[
-                  { required: true, message: 'Country/Region is required' },
-                ]}
-              >
-                <Select options={countryOptions} />
-              </NeatForm.Item>
-            </Col>
-            <Col {...responsiveColDef}>
-              <NeatForm.Item label="Time Zone" name="timeZone">
-                <Select options={timeZoneOptions} />
-              </NeatForm.Item>
-            </Col>
-          </Row>
-        </div>
-        <div className="form-profile__section">
-          <div className="form-profile__section-title">Contact Info</div>
-          <Row gutter={24}>
-            <Col {...responsiveColDef}>
-              <NeatForm.Item
-                label="Work Email"
-                name="workEmail"
-                rules={[{ required: true, message: 'Work Email is required' }]}
-              >
-                <Input disabled />
-              </NeatForm.Item>
-            </Col>
-            <Col {...responsiveColDef}>
-              <NeatForm.Item label="Secondary Email" name="secondaryEmail">
-                <Input />
-              </NeatForm.Item>
-            </Col>
-            <Col {...responsiveColDef}>
-              <NeatForm.Item label="Job Title" name="jobTitle">
-                <Input />
-              </NeatForm.Item>
-            </Col>
-          </Row>
-          <Row gutter={24}>
-            <Col {...responsiveColDef}>
-              <NeatForm.Item label="Phone Number" name="phoneNumber">
-                <Space>
+        <NeatForm
+          form={form}
+          layout="vertical"
+          className="form-profile__form"
+          initialValues={{
+            firstName: 'Felicia',
+            lastName: 'Lawson',
+            primaryLanguage: 'English',
+            country: 'China',
+            timeZone: 'Asia/Shanghai',
+            workEmail: 'felicia.lawson@derbysoft.net',
+            secondaryEmail: '',
+            jobTitle: 'Product Manager',
+            phoneNumber: '+87 18018600000',
+          }}
+        >
+          <div className="form-profile__section">
+            <div className="form-profile__section-title">Basic Info</div>
+            <div className="responsive-grid">
+              <div {...responsiveColDef}>
+                <NeatForm.Item
+                  label="First Name"
+                  name="firstName"
+                  rules={[
+                    { required: true, message: 'First Name is required' },
+                  ]}
+                >
+                  <Input />
+                </NeatForm.Item>
+              </div>
+              <div {...responsiveColDef}>
+                <NeatForm.Item
+                  label="Last Name"
+                  name="lastName"
+                  rules={[{ required: true, message: 'Last Name is required' }]}
+                >
+                  <Input />
+                </NeatForm.Item>
+              </div>
+              <div {...responsiveColDef}>
+                <NeatForm.Item label="Primary Language" name="primaryLanguage">
+                  <Select options={languageOptions} />
+                </NeatForm.Item>
+              </div>
+              <div {...responsiveColDef}>
+                <NeatForm.Item
+                  label="Country/Region"
+                  name="country"
+                  rules={[
+                    { required: true, message: 'Country/Region is required' },
+                  ]}
+                >
+                  <Select options={countryOptions} />
+                </NeatForm.Item>
+              </div>
+              <div {...responsiveColDef}>
+                <NeatForm.Item label="Time Zone" name="timeZone">
+                  <Select options={timeZoneOptions} />
+                </NeatForm.Item>
+              </div>
+            </div>
+          </div>
+          <div className="form-profile__section">
+            <div className="form-profile__section-title">Contact Info</div>
+            <Row gutter={24}>
+              <Col {...responsiveColDef}>
+                <NeatForm.Item
+                  label="Work Email"
+                  name="workEmail"
+                  rules={[
+                    { required: true, message: 'Work Email is required' },
+                  ]}
+                >
                   <Input disabled />
-                  {phoneVerified && <Tag color="success">Verified</Tag>}
-                  <Button
-                    type="text"
-                    icon={<span className="form-profile__icon-delete" />}
-                  />
-                </Space>
-              </NeatForm.Item>
-            </Col>
-          </Row>
-        </div>
-        <div className="form-profile__actions">
-          <Button>Cancel</Button>
-          <Button
-            type="primary"
-            onClick={handleUpdate}
-            className="form-profile__update-btn"
-          >
-            Update
-          </Button>
-        </div>
-      </NeatForm>
+                </NeatForm.Item>
+              </Col>
+              <Col {...responsiveColDef}>
+                <NeatForm.Item label="Secondary Email" name="secondaryEmail">
+                  <Input />
+                </NeatForm.Item>
+              </Col>
+              <Col {...responsiveColDef}>
+                <NeatForm.Item label="Job Title" name="jobTitle">
+                  <Input />
+                </NeatForm.Item>
+              </Col>
+            </Row>
+            <Row gutter={24}>
+              <Col {...responsiveColDef}>
+                <NeatForm.Item label="Phone Number" name="phoneNumber">
+                  <Space>
+                    <Input disabled />
+                    {phoneVerified && <Tag color="success">Verified</Tag>}
+                    <Button
+                      type="text"
+                      icon={<span className="form-profile__icon-delete" />}
+                    />
+                  </Space>
+                </NeatForm.Item>
+              </Col>
+            </Row>
+          </div>
+          <div className="form-profile__actions">
+            <Button>Cancel</Button>
+            <Button
+              type="primary"
+              onClick={handleUpdate}
+              className="form-profile__update-btn"
+            >
+              Update
+            </Button>
+          </div>
+        </NeatForm>
+      </ConfigProvider>
     </div>
   );
 };
