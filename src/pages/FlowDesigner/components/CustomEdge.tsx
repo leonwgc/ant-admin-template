6762b75b60e6c97aa9f670e5c9efd50e12125a3b
leonwgc <leon.wang@derbysoft.net>
2026-02-26 12:13:51 +0800

    fix: simplify CustomEdge component by removing delete button functionality and related code

--- src/pages/FlowDesigner/components/CustomEdge.tsx
+++ src/pages/FlowDesigner/components/CustomEdge.tsx
@@ -6,16 +6,11 @@ import React, { FC } from 'react';
 import {
   EdgeProps,
   getBezierPath,
-  EdgeLabelRenderer,
   BaseEdge,
-  useReactFlow,
 } from 'reactflow';
-import { CloseCircleFilled } from '@derbysoft/neat-design-icons';
-import './CustomEdge.scss';
 
 /**
  * 自定义连接线组件
- * 在连接线中间显示删除按钮
  */
 export const CustomEdge: FC<EdgeProps> = ({
   id,
@@ -28,8 +23,7 @@ export const CustomEdge: FC<EdgeProps> = ({
   style = {},
   markerEnd,
 }) => {
-  const { setEdges } = useReactFlow();
-  const [edgePath, labelX, labelY] = getBezierPath({
+  const [edgePath] = getBezierPath({
     sourceX,
     sourceY,
     sourcePosition,
@@ -38,28 +32,5 @@ export const CustomEdge: FC<EdgeProps> = ({
     targetPosition,
   });
 
-  const onEdgeClick = () => {
-    setEdges((edges) => edges.filter((edge) => edge.id !== id));
-  };
-
-  return (
-    <>
-      <BaseEdge path={edgePath} markerEnd={markerEnd} style={style} />
-      <EdgeLabelRenderer>
-        <div
-          style={{
-            position: 'absolute',
-            transform: `translate(-50%, -50%) translate(${labelX}px,${labelY}px)`,
-            pointerEvents: 'all',
-          }}
-          className="custom-edge-button"
-        >
-          <CloseCircleFilled
-            className="custom-edge-button__icon"
-            onClick={onEdgeClick}
-          />
-        </div>
-      </EdgeLabelRenderer>
-    </>
-  );
+  return <BaseEdge id={id} path={edgePath} markerEnd={markerEnd} style={style} />;
 };
